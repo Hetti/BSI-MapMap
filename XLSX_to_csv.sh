8db06031a7a3e16ae67751315ee6e0940cdcced6
Hetti <git@thepeko.at>
2021-03-05 20:30:09 +0100

    added IFS - DANKE SAP...

--- XLSX_to_csv.sh
+++ XLSX_to_csv.sh
@@ -3,14 +3,14 @@
 # Konvertiere alle XLSX zu CSV um sie durchsuchbar zu machen
 # Muss im Ordner ausgführt werden wo die Migrationstabellen Ordner vorhandne ist aus dem ZIP
 
-# 2 Files sind nicht konvertierbar, da sie ODS Files sind.
-# - OPS.2.2_Cloud-Nutzung.ods
-# - MT_Datenträgerarchiv.ods
 
 
 mkdir Migrationstabellen_CSV
 
-for csvfiles in Migrationstabellen/*
+# DANKE SAP - IFS auf Newline only wegen dem APP4.2 Dokument...
+IFS=$'\n'
+
+for csvfiles in Migrationstabellen/*.xlsx
 do
     #echo ${csvfiles}
     echo "Converting: `basename ${csvfiles}`"
@@ -18,6 +18,12 @@ do
     xlsx2csv2 "${csvfiles}" > Migrationstabellen_CSV/"${CSV_FILE}".csv
     echo "============================="
 done
+
+# 2 Files sind nicht mit dem xlsx2csv2 Tool konvertierbar, da sie ODS Files sind.
+# - OPS.2.2_Cloud-Nutzung.ods
+# - MT_Datenträgerarchiv.ods
+
+# TODO Diese zwei ODS Dateien in CSV konvertieren - Vielleicht mache ich das im Python File
 
 echo "DONE - bye bye"
 
